54d466eeda94b2e55f8921b57689d413ba5a3feb
Abdullah Nazir <a.nazir.hk@gmail.com>
2024-03-08 12:36:04 +0800

    Add files via upload

---
+++ finger1.py
@@ -0,0 +1,72 @@
+import time
+import serial
+
+startMarker = ''
+endMarker = '\n'
+dataStarted = False
+dataBuf = ""
+messageComplete = False
+
+
+
+def setupSerial(baudRate, serialPortName):
+    
+    global  serialPort
+    
+    serialPort = serial.Serial(port= serialPortName, baudrate = baudRate, timeout=0, rtscts=True)
+
+    print("Serial port " + serialPortName + " opened  Baudrate " + str(baudRate))
+
+    time.sleep(3)
+
+
+def sendToArduino(stringToSend):
+    
+        # this adds the start- and end-markers before sending
+    global startMarker, endMarker, serialPort
+    
+    stringWithMarkers = (startMarker)
+    stringWithMarkers += stringToSend
+    stringWithMarkers += (endMarker)
+
+    serialPort.write(stringWithMarkers.encode('utf-8')) # encode needed for Python3
+
+
+
+def recvLikeArduino():
+
+    global startMarker, endMarker, serialPort, dataStarted, dataBuf, messageComplete
+
+    if serialPort.inWaiting() > 0 and messageComplete == False:
+        x = serialPort.read().decode("utf-8") # decode needed for Python3
+        
+        if dataStarted == True:
+            if x != endMarker:
+                dataBuf = dataBuf + x
+            else:
+                dataStarted = False
+                messageComplete = True
+        elif x == startMarker:
+            dataBuf = ''
+            dataStarted = True
+    
+    if (messageComplete == True):
+        messageComplete = False
+        return dataBuf
+    else:
+        return "XXX" 
+
+
+
+if __name__ == '__main__':
+
+    setupSerial(115200, "COM3")
+
+    while True:
+
+        cmd = input("User command: ")
+
+        sendToArduino(cmd)
+
+        time.sleep(0.5)
+
---
+++ main.py
@@ -0,0 +1,24 @@
+import finger1 #roller finger
+import finger2 #direct-drive finger
+import time
+
+
+if __name__ == '__main__':
+
+    finger1.setupSerial(115200, 'COM3')
+    finger2.setupSerial(115200, 'COM18')
+
+    while True:
+
+        cmd = input("User command: ")
+
+        if cmd[0] == 'S' or cmd[0] == 'D':
+            finger1.sendToArduino(cmd)
+
+        elif cmd[0] == 'T':
+            finger2.sendToArduino(cmd)
+
+        else:
+            print("Invalid command")
+
+        time.sleep(0.5)
