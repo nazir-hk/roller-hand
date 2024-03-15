c7c58ab434b806d42821217e5f4e4e9a70810752
Abdullah Nazir <a.nazir.hk@gmail.com>
2024-03-15 13:17:19 +0800

    Add files via upload

--- gripper_new.py
+++ gripper_new.py
@@ -4,11 +4,18 @@ import time
 
 import msvcrt
 
+
 def open():
     finger2.sendCommand("T-0.3")
 
 def close():
     finger2.sendCommand("T-0.8")
+
+def lowStiffness():
+    finger2.sendCommand("S5")
+
+def highStiffness():
+    finger2.sendCommand("S250")
 
 def twirl():
     finger2.sendCommand("T0.45")
@@ -20,7 +27,7 @@ def rollerH():
     finger1.sendCommand("S25")
 
 def rollerV():
-    finger1.sendCommand("S120")
+    finger1.sendCommand("S123")
 
 def reorientTip(angle):
     finger1.sendCommand("S"+angle)
@@ -28,7 +35,13 @@ def reorientTip(angle):
 if __name__ == '__main__':
 
     finger1.setupSerial(115200, 'COM3')
-    finger2.setupSerial(115200, 'COM18')
+    finger2.setupSerial(115200, 'COM5')
+
+    time.sleep(2)
+
+
+    open()
+    time.sleep(1)
 
     roller_angle = 0
     servo_angle = 25 #initial servo angle
@@ -36,7 +49,7 @@ if __name__ == '__main__':
     while True:
 
         # cmd = input("0: open\n1: close\n2: twirl\n3: rollTip\n4: rollerH\n5: rollerV\n")
-        print("0: open\n1: close\n2: twirl\n3: rollerH\n5: rollerV\n")
+        print("0: open\n1: close\n2: twirl\n3: rollerH\n4: rollerV\n5: low stiffness\n6: high stiffness")
 
         cmd = msvcrt.getch()
 
@@ -55,8 +68,13 @@ if __name__ == '__main__':
         elif cmd.upper() == b"4":
             rollerV()
             
+        elif cmd.upper() == b"5":
+            lowStiffness()
 
-        elif cmd == b'\xe0':  # Arrow key prefix
+        elif cmd.upper() == b"6":
+            highStiffness()
+
+        elif cmd == b'\x00':  # Arrow key prefix
             arrow_key = msvcrt.getch()
             if arrow_key == b'H':
                 roller_angle = roller_angle + 20
@@ -67,11 +85,11 @@ if __name__ == '__main__':
                 rollTip(str(roller_angle))
 
             elif arrow_key == b'M':
-                servo_angle = servo_angle + 5
+                servo_angle = servo_angle + 1
                 reorientTip(str(servo_angle))
 
             elif arrow_key == b'K':
-                servo_angle = servo_angle - 5
+                servo_angle = servo_angle - 1
                 reorientTip(str(servo_angle))
             else:
                 print("Invalid command")  
